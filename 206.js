0dc805d460f93af39e6d9295147c78064ebc8447
jimmy.chou <nkai940372@gmail.com>
2024-07-02 15:57:25 +0800

    feat: add 206

---
+++ 206.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+function ListNode(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+}
+
+const head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+
+var reverseList = function (head) {
+    let prev = null;
+    let current = head;
+
+    while (current !== null) {
+        let nextTemp = current.next;
+        current.next = prev;
+        prev = current;
+        current = nextTemp;
+    }
+    return prev;
+};
+
+console.log(reverseList(head));

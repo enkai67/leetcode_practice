e0b675af9c55ffc41a86b45ec7d850245663ebed
jimmy.chou <nkai940372@gmail.com>
2024-07-24 23:03:30 +0800

    feat: add 283

---
+++ 283.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+// reference solution
+var moveZeroes = function (nums) {
+    let left = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] !== 0) {
+            [nums[right], nums[left]] = [nums[left], nums[right]];
+            left++;
+        }
+    }
+
+    return nums;
+};
